98508f4da9e58bc21b8fd8954170db95ea125eba
waterfish0129 <47707929+waterfish0129@users.noreply.github.com>
2026-01-07 14:34:55 +0800

    刪除多於檔案

--- service/dto/UserLoginDTO.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type UserLoginDTO struct {
-	Name     string `json:"name" binding:"required,email" message:"必填哦" email_err:"格式要信箱"`
-	Password string `json:"password" binding:"required" message:"必填哦"`
-}
